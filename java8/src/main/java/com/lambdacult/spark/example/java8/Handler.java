0f09e99aad1756470052f47cbb8b29c1c244d4c9
ben <ben.romberg@gmx.de>
2017-02-20 09:48:31 +0100

    extract WarmupService

--- java8/src/main/java/com/lambdacult/spark/example/java8/Handler.java
+++ java8/src/main/java/com/lambdacult/spark/example/java8/Handler.java
@@ -4,11 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
     private static final Logger LOG = Logger.getLogger(Handler.class);
@@ -16,48 +12,23 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
         LOG.info("received: " + input);
-        Object responseBody = buildResponseForInput(input, context);
+        Object responseBody = buildResponseBody(input, context);
         return ApiGatewayResponse.builder()
                 .setStatusCode(200)
                 .setObjectBody(responseBody)
                 .build();
     }
 
-    private Object buildResponseForInput(Map<String, Object> input, Context context) {
-        if (Actions.WARMUP.equals(input.get(Parameters.ACTION))) {
-            return buildWarmupResponse(input, context);
-        }
-        return new Response("Go Serverless v1.x! Your function executed successfully!", input);
-    }
-
-    private Object buildWarmupResponse(Map<String, Object> input, Context context) {
+    private Object buildResponseBody(Map<String, Object> input, Context context) {
         try {
-            long minimumDurationMilliseconds = ((Number) input.get(Parameters.MINIMUM_DURATION_MILLISECONDS)).longValue();
-            sleepMinimumDuration(minimumDurationMilliseconds);
-            String uptime = readFileToString("/proc/uptime");
-            String bootId = readFileToString("/proc/sys/kernel/random/boot_id");
-            return new WarmupResponse(uptime, bootId, context.getLogStreamName());
+            if (input.get("action").equals("warmup")) {
+                long minimumDurationMilliseconds = ((Number) input.get("minimumDurationMilliseconds")).longValue();
+                return new WarmupService().warmup(minimumDurationMilliseconds, context.getLogStreamName());
+            }
+            return new Response("Go Serverless v1.x! Your function executed successfully!", input);
         } catch (Exception e) {
-            return new Response("Exception occurred: " + e.getMessage(), input);
+            LOG.error("Couldn't build Response", e);
+            throw new RuntimeException(e);
         }
-    }
-
-    private void sleepMinimumDuration(long minimumDurationMilliseconds) throws InterruptedException {
-        LOG.info("sleeping for " + minimumDurationMilliseconds + "ms");
-        Thread.sleep(minimumDurationMilliseconds);
-        LOG.info("sleeping done");
-    }
-
-    private String readFileToString(String path) throws IOException {
-        return Files.readAllLines(Paths.get(path)).stream().collect(Collectors.joining());
-    }
-
-    private interface Parameters {
-        String ACTION = "action";
-        String MINIMUM_DURATION_MILLISECONDS = "minimumDurationMilliseconds";
-    }
-
-    private interface Actions {
-        String WARMUP = "warmup";
     }
 }
